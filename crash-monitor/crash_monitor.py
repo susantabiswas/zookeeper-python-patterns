0dc0c3356a19b4b11e1f3ea3b3411b6922e97c26
Susanta <17782600+susantabiswas@users.noreply.github.com>
2023-12-07 20:47:18 +0530

    Crash detection using zookeeper

---
+++ crash-monitor/crash_monitor.py
@@ -0,0 +1,84 @@
+from kazoo.client import KazooClient
+import argparse
+from datetime import datetime
+import time
+
+class CrashMonitor:
+    def __init__(self, zookeeper_hosts, heartbeat_path) -> None:
+        self.zookeeper = KazooClient(hosts=zookeeper_hosts)
+        self.heartbeat_path = heartbeat_path
+        self.monitor_path = '/monitors'
+        self.connect()
+        self.instances = set()
+
+    def connect(self) -> None:
+        self.zookeeper.start()
+        # If the heartbeat path doesn't exists, create the path
+        self.zookeeper.ensure_path(self.heartbeat_path)
+
+        # register the monitor node
+        self.zookeeper.create("{}/{}_".format(self.monitor_path, 'monitor'),
+                              ephemeral=True, sequence=True, makepath=True)
+        
+        # get the list of instances to be monitored
+        self.instances = set(self.zookeeper.get_children(self.heartbeat_path))
+        print('[Startup] Found {} instances. Monitoring started...'.format(len(self.instances)))
+        print(self.instances)
+
+    def __del__(self) -> None:
+        self.zookeeper.close()
+
+    def send_alert(self, node_id) -> None:
+        print("Node {} is down, alert sent!!".format(node_id))
+
+    def monitor(self) -> None:
+        # add a watcher to monitor the znode path where heartbeat of instances are saved
+        # Everytime there is any modification in the znode path, the watch_nodes function
+        # will be called
+        self.zookeeper.ChildrenWatch(path=self.heartbeat_path, func=self.watch_nodes)
+        print('Monitoring application nodes...')
+
+    def watch_nodes(self, children) -> None:
+        print("\nTimestamp: {0}".format((datetime.now()).strftime("%B %d, %Y %H:%M:%S")))
+        print('Change detected in application nodes...')
+
+        children = set(children)
+        # any instance that is not in the children list is offline
+        offline_instances = self.instances - children
+        # any instance in children list that is not in currently tracked instances
+        # is a new instance
+        new_instances = children - self.instances
+
+        print('******* Node Status')
+        for child in children:
+            print('Node {} status: {}'.format(child,
+                                            'ONLINE' if child in self.instances else 'OFFLINE'))
+
+        print('******* New instances found: {}'.format(len(new_instances)))
+        for instance in new_instances:
+            # new instance found
+            self.instances.add(child)
+            print('New instance found: {}'.format(instance))
+
+        print('******* Offline instances found: {}'.format(len(offline_instances)))
+        for instance in offline_instances:
+            # stop tracking the offline instance and send an alert
+            self.send_alert(instance)
+            self.instances.remove(instance)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Application heartbeat monitor")
+    parser.add_argument('--hosts', type=str, help='Zookeeper hosts',
+                        default='localhost:2182,localhost:2183,localhost:2184')
+    parser.add_argument('--heartbeat_path', type=str,
+                        help='Zookeeper heartbeat path', default='/nodes')
+
+    args = parser.parse_args()
+
+    node_heartbeat = CrashMonitor(zookeeper_hosts=args.hosts,
+                                  heartbeat_path=args.heartbeat_path)
+    node_heartbeat.monitor()
+
+    while True:
+        time.sleep(10)
---
+++ crash-monitor/worker.py
@@ -0,0 +1,61 @@
+"""
+    Runs on the worker server.
+"""
+
+import argparse
+from kazoo.client import KazooClient
+import uuid
+import time
+
+class NodeHeartBeat:
+    """
+        Sends heartbeat to the zookeeper server.
+    """
+    def __init__(self, zookeeper_hosts, node_id, heartbeat_path='/nodes') -> None:
+        self.zookeeper = KazooClient(hosts=zookeeper_hosts)
+        self.heartbeat_path = heartbeat_path
+
+        # If the instance id is not provide, we create one for it        
+        if node_id is None:
+            self.node_id = uuid.uuid1()
+
+    def connect(self) -> None:
+        self.zookeeper.start()
+
+        # If the heartbeat path doesn't exists, create the path
+        self.zookeeper.ensure_path(self.heartbeat_path)
+
+        # Create an ephimeral znode for the current node
+        # If the node is down, the ephimeral znode will be deleted as 
+        # well. The monitoring node will be notified of this event and
+        # can take action.
+        self.zookeeper.create(
+            self.heartbeat_path + "/" + str(self.node_id), ephemeral=True)
+        
+        print('Heartbeat connection sent to zookeeper server for node: {}'
+              .format(self.node_id))
+
+    def disconnect(self) -> None:
+        self.zookeeper.close()
+
+    def __del__(self) -> None:
+        self.zookeeper.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Applicaion heartbeat daemon")
+    parser.add_argument('--hosts', type=str, help='Zookeeper hosts',
+                        default='localhost:2182,localhost:2183,localhost:2184')
+    parser.add_argument('--heartbeat_path', type=str,
+                        help='Zookeeper heartbeat path', default='/nodes')
+    parser.add_argument('--node_id', type=str, help='Node ID', default=None)
+
+    args = parser.parse_args()
+
+    node_heartbeat = NodeHeartBeat(zookeeper_hosts=args.hosts,
+                                   node_id=args.node_id,
+                                   heartbeat_path=args.heartbeat_path)
+    node_heartbeat.connect()
+
+    while True:
+        time.sleep(10)
